fe45e579b6041dc1f07ca4903f89dc6e2c4a79c8
AlexM4xi <alexesquivel136@gmail.com>
2025-01-25 08:55:49 -0300

    1er modificando pruebas.py

---
+++ pruebas.py
@@ -0,0 +1,26 @@
+'''
+	this file be of test for algoritm
+'''
+import random
+
+n = ''
+
+for i in range(8):
+	n += str(random.randint(0,9))
+
+'''
+	introducir cada numero de n en una lista,
+	rrecorrer la misma y reemplazar el 3er
+	digito sabiendo que las opciones son 3
+	4, 5 y 6, si el 3er digito es 4 se reemplaza
+	por un 5 o si es 5 el reemlazo es 6 o
+	si es 6, el cambio es por un 4, imprimir
+	en pantalla el numero completo antes y
+	despues de reemplazarlo.
+
+'''
+for num in n:
+	print(num.split(),end='')
+
+print()
+
